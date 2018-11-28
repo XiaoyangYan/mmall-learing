cbcb76a2d576d7222d4661c5d04cad5e452ea844
XiaoyangYan <z5167310@ad.unsw.edu.au>
2018-11-28 12:39:59 +1100

    modified

---
+++ src/main/java/java/com/mmall/dao/PayInfoMapper.java
@@ -0,0 +1,15 @@
+package java.com.mmall.dao;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
